ec225e02a612e06aabe8231bb8bde349d15e8c7b
Christian Gigena <nahuel.gigena@hotmail.com>
2025-08-30 23:49:44 -0300

    MODIFY:  Readme

--- obj/Debug/net8.0-windows/Kiosco-La-esquina.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/Kiosco-La-esquina.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Kiosco-La-esquina")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2b37a8a35ead083c0f907fa0ff4b8726c103f436")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+247c469fa21b0b53ef6c33d211156b8b09e3a1a5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Kiosco-La-esquina")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Kiosco-La-esquina")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
